9883fcb16088acc5692233902ceba940a7d219e2
Chris Mikkelsen <yubh8n.sdu@gmail.com>
2020-03-02 12:46:13 +0100

    added stuff!

--- startup_script.sh
+++ startup_script.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
+
+export PX4_HOME_LAT=55.47228089795564
+export PX4_HOME_LON=10.32437208048389
+export PX4_HOME_ALT=0.0
+
 cd Firmware/
 
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
