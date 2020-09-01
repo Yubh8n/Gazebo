c1cf9dd46518b138068fe1d0c628fa7808d8c0da
yubh8n <yubh8n.sdu@gmail.com>
2020-09-01 10:33:19 +0200

    various updates to the scripts, launch files etc.

--- build_firmware.sh
+++ build_firmware.sh
@@ -1,9 +1,18 @@
-# /!bin/bash
-cp Specials/{marker.world,mark.world} Firmware/Tools/sitl_gazebo/worlds/
+#! /bin/bash
+# Worlds
+cp Specials/{marker.world,infront_spawn.world,cal.world} Firmware/Tools/sitl_gazebo/worlds/
+
+# Boards.
 cp -r Specials/{aruco_marker,calibration_chessboard} Firmware/Tools/sitl_gazebo/models/
-cp Specials/posix_sitl_cust.launch Firmware/launch/posix_sitl_cust.launch
+
+# Launch files
+cp Specials/{posix_sitl_cust.launch,marker_test.launch,posix_sitl_calibration.launch} Firmware/launch/
+
+# Iris files
 mv Firmware/Tools/sitl_gazebo/models/rotors_description/urdf/iris_base.xacro Firmware/Tools/sitl_gazebo/models/rotors_description/urdf/iris_base_copy.xacro
 cp Specials/iris_base.xacro Firmware/Tools/sitl_gazebo/models/rotors_description/urdf/
-cd Firmware/
+
+# Firmware build.
+cd Firmware/ || return
 make clean &&
 make px4_sitl gazebo
--- cal_startup_script.sh
+++ cal_startup_script.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
-cd Firmware/
+cd Firmware/ || exit
 
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
-cd ..
 
 roslaunch px4 posix_sitl_calibration.launch
