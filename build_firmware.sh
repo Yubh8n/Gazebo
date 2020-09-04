142b3851cb7c8f985cabaed95a2d60ba2b76cae4
yubh8n <yubh8n.sdu@gmail.com>
2020-09-04 13:39:22 +0200

    Stuff

--- build_firmware.sh
+++ build_firmware.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 # Worlds
-cp Specials/{marker.world,infront_spawn.world,cal.world} Firmware/Tools/sitl_gazebo/worlds/
+cp -rf Specials/{marker.world,infront_spawn.world,cal.world} Firmware/Tools/sitl_gazebo/worlds/
 
 # Boards.
-cp -r Specials/{aruco_marker,calibration_chessboard} Firmware/Tools/sitl_gazebo/models/
+cp -rf  Specials/{aruco_marker,calibration_chessboard} Firmware/Tools/sitl_gazebo/models/
 
 # Launch files
-cp Specials/{posix_sitl_cust.launch,marker_test.launch,posix_sitl_calibration.launch} Firmware/launch/
+cp -rf Specials/{posix_sitl_cust.launch,marker_test.launch,posix_sitl_calibration.launch} Firmware/launch/
 
 # Iris files
 mv Firmware/Tools/sitl_gazebo/models/rotors_description/urdf/iris_base.xacro Firmware/Tools/sitl_gazebo/models/rotors_description/urdf/iris_base_copy.xacro
-cp Specials/iris_base.xacro Firmware/Tools/sitl_gazebo/models/rotors_description/urdf/
+cp -rf Specials/iris_base.xacro Firmware/Tools/sitl_gazebo/models/rotors_description/urdf/
 
 # Firmware build.
 cd Firmware/ || return
