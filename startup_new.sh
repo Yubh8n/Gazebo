b825e494044a3c4806e097825da121ae0a4b8996
yubh8n <yubh8n.sdu@gmail.com>
2020-11-16 10:36:49 +0100

    adding new version of Firmware (PX4-Autopilot)

--- startup_new.sh
+++ startup_new.sh
@@ -5,4 +5,4 @@ source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
 
-roslaunch px4 posix_sitl_calibration.launch
+roslaunch px4 posix_sitl_newcust.launch
